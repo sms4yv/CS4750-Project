4514514c682b958c9cb9a4a29aab083c414ba5d7
sms4yv <64984826+sms4yv@users.noreply.github.com>
2022-04-17 01:26:46 -0400

    Added Textbook search by ISBN and class

--- reqTextbook.php
+++ reqTextbook.php
@@ -5,24 +5,23 @@
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-   // echo "POST CALLED";
-    //if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "submit")
-    //{  
-       // echo "Search presses";
+    //echo "POST CALLED"
+        //echo "Search presses";
        // header("Location: searchResults.php");
         
       // If the button is clicked and its value is "submit" then call getTextbook() function
-      $results = getTextbook($_POST['search']);
+      $results = getReqTextbook($_POST['ID'],$_POST['dept']);
      // header("Location: searchResults.php");
     //}
 }
-function getTextbook($search){
+function getReqTextbook($ID, $dept){
     global $db;
     //echo "preparing statement with ".$search." as primary key";
-    $query = "SELECT * FROM `sells` WHERE `ISBN` = (:ISBN)";
+    $query = "SELECT `ISBN`, `title`, `author`, `sname`, `price` FROM `has` NATURAL JOIN `Textbook` NATURAL JOIN `sells` WHERE `courseID` = (:courseID) AND `dept` = (:dept);";
     //echo $query;
     $statement = $db->prepare($query);
-    $statement->bindValue(':ISBN', $search);
+    $statement->bindValue(':courseID', $ID);
+    $statement->bindValue(':dept', $dept);
 	$statement->execute();  
 
     $results = $statement->fetchAll();
@@ -51,7 +50,7 @@ function getTextbook($search){
   <meta name="eal8hs, sms4yv, btg6zn" content="CS 4750 Project">
   <meta name="Textbook Seller" content="Page to create student profile">  
     
-  <title>Find a Textbook by ISBN</title>
+  <title>Required texts by class</title>
   
   <!-- 3. link bootstrap -->
   <!-- if you choose to use CDN for CSS bootstrap -->  
@@ -81,32 +80,37 @@ function getTextbook($search){
     <body>
     <?php include('header.html') ?>
     <?php session_start(); ?>
-        <h1> Search By Textbook</h1>
+        <h1> Search for Textbooks by class</h1>
         <form name="mainForm" method="post">
         <div class="row mb-3 mx-3">
-        <input type="text" class="form-control" name="search" placeholder="Enter the Textbook ISBN"/>
-        <input type="submit" value="Search" name="btnAction" class="btn btn-dark" 
-        title="Find Textbook" />  
+        <input type="text" class="form-control" name="ID" placeholder="Enter the course ID (i.e 4750)"/>
+    </div>
+        <div class="row mb-3 mx-3">
+        <input type="text" class="form-control" name="dept"placeholder="Enter the dept code (i.e CS or PHYS)"/>
         </div>
+        <input type="submit" value="Search" name="btnAction" class="btn btn-dark" 
+        title="Search" />  
         </form>  
-        <h4>Results: </h4>
+        <h4>Required Textbooks for : <?php $search?> </h4>
         <table class="w3-table w3-bordered w3-card-4" style="width:90%">
         <thead>
         <tr style="background-color:#B0B0B0">
-            <th width="25%">Seller</th>
-            <th width="25%">ISBN</th>
-            <th width="15%">Price</th>
-            <th width="15%">Type ?</th>
+            <th width="20%">ISBN</th>
+            <th width="25%">Title</th>
+            <th width="15%">Author</th>
+            <th width="10%">Price</th>
+            <th width="15%">Seller</th>
         </tr>
         </thead>
         <?php
         if($results !== null){
         foreach($results as $entry){?>
          <tr>
-         <td><?php echo $entry['sname']; ?></td>
-        <td><?php echo $entry['ISBN']; ?></td>
-        <td><?php echo $entry['price']; ?></td>
-        <td><?php echo $entry['book_type']?></td>
+         <td><?php echo $entry['ISBN']; ?></td>
+        <td><?php echo $entry['title']; ?></td>
+        <td><?php echo $entry['author']; ?></td>
+        <td><?php echo $entry['price'];?></td>
+        <td><?php echo $entry['sname']; ?></td>
         <tr>
         <?php }}; ?>
         </table>
